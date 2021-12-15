5401fab207a4c34ce69c75390b4927f098315bcf
TimasTT <timofey.konoplyov@gmail.com>
2021-12-16 00:43:48 +0300

    add tests

---
+++ src/task_3/test/backTest/backTest.cpp
@@ -0,0 +1,18 @@
+//
+// Created by timas on 16.12.2021.
+//
+
+#include <gtest/gtest.h>
+
+#include "mission.hpp"
+#include "operation.hpp"
+#include "position.hpp"
+
+TEST(OperationTests, SuccessOperation) {
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
